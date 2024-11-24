bf6e7d04db922de6ee96f5d8501cf50bc1d5ff80
annblle-e <sm4rt.dogs@gmail.com>
2024-11-24 16:45:59 +0700

    kk

--- server.js
+++ server.js
@@ -23,9 +23,8 @@ app.get('/search', async (req, res) => {
         // Meluncurkan Puppeteer dan membuka browser
         const browser = await puppeteer.launch({
             headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']  // Crucial for production environments
+            args: ['--disable-http2']  // Menonaktifkan HTTP/2
         });
-        
         const page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
         await page.goto(url, { waitUntil: 'domcontentloaded' });
